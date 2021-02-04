2fe5a098d28c7066ec610bfc314546766ecc0e21
ssomagani <ssomagani@voltdb.com>
2021-02-04 05:11:51 -0500

    Fixed issue in serializing timestamps

--- events/metro/serde/TrainEventSer.java
+++ events/metro/serde/TrainEventSer.java
@@ -6,7 +6,7 @@ import metro.TrainEvent;
 
 public class TrainEventSer implements org.apache.kafka.common.serialization.Serializer<TrainEvent> {
 
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-DD hh:mm:ss.SSSSSS");
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSSSSS");
 
     @Override
     public byte[] serialize(String arg0, TrainEvent trainEvent) {
@@ -16,7 +16,7 @@ public class TrainEventSer implements org.apache.kafka.common.serialization.Seri
                     trainEvent.trainId + ","
                             + trainEvent.location.station.stationId + ","
                             + trainEvent.location.state.value + ","
-                            +trainEvent.location.timestamp.format(formatter)).getBytes();
+                            + trainEvent.location.timestamp.format(formatter)).getBytes();
         } catch (Exception e) {
             e.printStackTrace();
         }
